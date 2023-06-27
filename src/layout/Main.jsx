e4f5646154dbbb820aa036edcbc73f3b40a00a30
istiakantu <istakahmed636@gmail.com>
2023-06-28 05:21:41 +0600

    navbar added

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import Home from "../components/Home/Home";
+import Navbar from "../components/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Main from './layout/Main';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
